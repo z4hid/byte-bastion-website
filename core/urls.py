1b018407f8fbd1a932e692df0815ed7701a72be5
z4hid <zahidhasan9297@gmail.com>
2024-09-10 19:51:43 +0600

    updated home and urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomepageView, AboutPageView, DownloadPageView
+from .views import HomepageView, AboutPageView, DownloadPageView, ProductPageView
 
 urlpatterns = [
     path('', HomepageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('download/', DownloadPageView.as_view(), name='download')
+    path('download/', DownloadPageView.as_view(), name='download'),
+    path('product/', ProductPageView.as_view(), name='product'),
 ]
