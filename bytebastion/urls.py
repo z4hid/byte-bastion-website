b2dd80b23184c571702fc60a39218996af0bf65a
z4hid <zahidhasan9297@gmail.com>
2024-09-13 20:26:22 +0600

    grappelli url added

--- bytebastion/urls.py
+++ bytebastion/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
