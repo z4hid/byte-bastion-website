85c1f51b5ffc6a90a2c4d39cfa257087571c41bc
z4hid <zahidhasan9297@gmail.com>
2024-09-17 19:53:24 +0600

    detect app added

--- detect/admin.py
+++ detect/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import APIRequestLog
 
-# Register your models here.
+@admin.register(APIRequestLog)
+class APIRequestLogAdmin(admin.ModelAdmin):
+    list_display = ('image_name', 'timestamp', 'response_status', 'response_data', 'error_message')
+    list_filter = ('timestamp', 'response_status')
+    search_fields = ('image_name', 'response_data', 'error_message')
